a2e3ba5e2ccbccd9e3c2081c7da4e88682a5c4e8
Arnie Weber <arnieweb@gmail.com>
2023-07-28 16:40:07 -0400

    update bangbang65 keymaps

--- keyboards/bangbang/bangbang65/keymaps/default/keymap.c
+++ keyboards/bangbang/bangbang65/keymaps/default/keymap.c
@@ -34,17 +34,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 *	  K40, K41, K42,  K43,  K44, K45,          K90, K91,  K92,  K93, K94,   K95, K96, K97  \
 */
 	[_QW] = LAYOUT( /* QWERTY */
-		QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-		KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
-		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP
-		KC_LCTL, KC_LGUI, KC_LALT, MO(_RS), KC_SPC,  KC_NO,            KC_ENT,  KC_BSPC, MO(_LW), KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+		QK_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+		KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+		CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+		KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP
+		KC_LCTL,        KC_LGUI, KC_LALT, MO(_RS), KC_SPC,  KC_NO,            KC_ENT,  KC_BSPC, MO(_LW), KC_RALT, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
 		),
 
 	[_RS] = LAYOUT(	/* [> RAISE <] */
 		KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,
-		KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO,   KC_HOME, KC_UP,   KC_END,  KC_NO,   KC_NO,   KC_NO,   KC_NO,
-		KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,   KC_TRNS,
+		KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_NO,   KC_NO,   KC_NO,
+		KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_NO,   KC_TRNS,
 		KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_PGUP,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,            KC_TRNS, KC_DEL,  MO(_RL), KC_TRNS, TG(_NP), KC_HOME, KC_PGDN, KC_END
 		),
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		),
 
 	[_RL] = LAYOUT(	/* [> RAISE <] + [> LOWER <] */
-		RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   RESET,
+		RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
 		KC_NO,   RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
 		KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
 		KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
@@ -74,12 +74,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		),
 };
 
+
+/* Encoder rotation mapping */
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [1] =   { ENCODER_CCW_CW(KC_TRNS,  KC_TRNS) },
-    [1] =   { ENCODER_CCW_CW(KC_TRNS,  KC_TRNS) },
-    [2] =   { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD) },
+    [1] =   { ENCODER_CCW_CW(C(S(KC_TAB)),  LCTL(KC_TAB)) },
+    [2] =   { ENCODER_CCW_CW(KC_TRNS,  KC_TRNS) },
+    [3] =   { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD) },
     [4] =   { ENCODER_CCW_CW(KC_NO,    KC_NO  ) }
 };
 #endif
