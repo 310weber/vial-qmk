3c8261e43ffe655ded93dcf87146be9bf0649c2e
310weber <arnieweb@gmail.com>
2023-12-08 22:26:40 -0500

    add bangbang1 - untested

---
+++ keyboards/bangbang/bangbang1/keymaps/vial/rules.mk
@@ -0,0 +1,8 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+QMK_SETTINGS = yes
+ENCODER_MAP_ENABLE = yes
+VIAL_ENCODERS_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+# must set insecure since one key can't unlock
+VIAL_INSECURE = yes
---
+++ keyboards/bangbang/bangbang1/ver1/config.h
@@ -0,0 +1,29 @@
+/* Copyright 2021 Arnie Weber
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+
+/* Settings should be configured in info.json where possible.
+ * Below settings are not defined in info.json.
+ */
+
+/* RGB backlight defaults */
+#define RGBLIGHT_DEFAULT_MODE	RGB_MODE_RAINBOW
+#define RGBLIGHT_DEFAULT_HUE	100
+#define RGBLIGHT_DEFAULT_SAT	255
+#define RGBLIGHT_DEFAULT_VAL	RGBLIGHT_LIMIT_VAL
+#define RGBLIGHT_DEFAULT_SPD	0
